b643b583374937bd5dee8d6798fe97d8fb2030e9
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-08-17 13:18:37 -0300

    update route and onboarding url

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 fission spec init
 fission env create --spec --name identifier-document-env --image nexus.sigame.com.br/fission-async:0.1.6 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
 fission fn create --spec --name identifier-document-fn --env identifier-document-env --src "./func/*" --entrypoint main.save_document --executortype newdeploy --maxscale 1
-fission route create --spec --name identifier-document-rt --method POST --url /onboarding/post-identifier-document --function identifier-document-fn
+fission route create --spec --name identifier-document-rt --method POST --url /onboarding/post_identifier_document --function identifier-document-fn
